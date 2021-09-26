4174b617e61da3f9f3b4c0e52ece64685128f3ba
Brandon Twilley <brandon.twilley@okstate.edu>
2021-09-26 00:31:31 -0500

    Removed dead code.

--- linked_list_rc/src/linked_list.rs
+++ linked_list_rc/src/linked_list.rs
@@ -5,11 +5,6 @@ pub struct Node {
     pub after: Option<Rc<RefCell<Node>>>,
 }
 
-#[derive(Clone)]
-pub struct Runner {
-    pub current_node: Option<Rc<RefCell<Node>>>,
-}
-
 pub fn new_node(s: String) -> Rc<RefCell<Node>> {
     Rc::new(RefCell::new(Node {
         before: None,
@@ -20,12 +15,6 @@ pub fn new_node(s: String) -> Rc<RefCell<Node>> {
 pub fn join(bef: Rc<RefCell<Node>>, aft: Rc<RefCell<Node>>) {
     bef.borrow_mut().after = Some(aft.clone());
     aft.borrow_mut().before = Some(bef.clone());
-}
-
-pub fn new_runner(n: Rc<RefCell<Node>>) -> Runner {
-    Runner {
-        current_node: Some(n.clone()),
-    }
 }
 
 pub fn next(r: Rc<RefCell<Node>>) -> Option<Rc<RefCell<Node>>> {
