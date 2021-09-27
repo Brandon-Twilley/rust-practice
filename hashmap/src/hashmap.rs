07c8fbe1a4fe4eac591612e08e0d9cc72fae0ab2
Brandon Twilley <brandon.twilley@okstate.edu>
2021-09-27 02:11:21 -0500

    Finished hash map :D

--- hashmap/src/hashmap.rs
+++ hashmap/src/hashmap.rs
@@ -8,7 +8,7 @@ pub struct Node<T> {
 
 pub fn new_node<T>(obj: Rc<RefCell<T>>) -> Rc<RefCell<Node<T>>> {
     let mut n: Vec<Option<Rc<RefCell<Node<T>>>>> = vec![];
-    for _ in 0..51 {
+    for _ in 0..52 {
         n.push(None)
     }
     Rc::new(RefCell::new(Node {
@@ -19,7 +19,7 @@ pub fn new_node<T>(obj: Rc<RefCell<T>>) -> Rc<RefCell<Node<T>>> {
 
 pub fn empty_node<T>() -> Rc<RefCell<Node<T>>> {
     let mut n: Vec<Option<Rc<RefCell<Node<T>>>>> = vec![];
-    for _ in 0..51 {
+    for _ in 0..52 {
         n.push(None)
     }
     Rc::new(RefCell::new(Node {
@@ -37,27 +37,27 @@ impl<T> Clone for Node<T> {
     }
 }
 
-impl<T> Node<T> {
+impl<T: std::fmt::Debug> Node<T> {
     pub fn push(&mut self, s: String, obj: Rc<RefCell<T>>) -> bool {
         match s.get(..1) {
             Some(a) => match s.get(1..) {
-                Some(b) => self.char_switch(a, b, obj),
-                None => self.char_switch(a, "", obj),
+                Some(b) => self.char_switch_push(a, b, obj),
+                None => self.char_switch_push(a, "", obj),
             },
             None => self.set_object(obj),
         }
     }
     // Pushes the given node onto the vector list
-    fn char_switch(&mut self, index: &str, rest: &str, obj: Rc<RefCell<T>>) -> bool {
+    fn char_switch_push(&mut self, index: &str, rest: &str, obj: Rc<RefCell<T>>) -> bool {
         match index.chars().nth(0).unwrap() {
             //If our index character is a capital letter
-            'A'..='Z' => self.normalize_char(index, rest, 65, obj),
+            'A'..='Z' => self.normalize_char_push(index, rest, 65, obj),
             //If our index character is a capital letter
-            'a'..='z' => self.normalize_char(index, rest, 71, obj),
+            'a'..='z' => self.normalize_char_push(index, rest, 71, obj),
             _ => false,
         }
     }
-    fn normalize_char(
+    fn normalize_char_push(
         &mut self,
         index: &str,
         rest: &str,
@@ -69,23 +69,16 @@ impl<T> Node<T> {
             //If our selected node already exists
             Some(node) => node.borrow_mut().push(rest.to_string(), obj.clone()),
             // if our selected node doesn't already exist
-            None => {
-                    self.set_index(normalized, new_node(obj.clone()));
-                    self.normalize_char(index, rest, normalize_num, obj.clone())
+            None => match rest.len() {
+                0 => {
+                    self.next[normalized].replace(new_node(obj.clone()));
+                    true
                 }
-            // None => match rest.len() {
-            //     0 => self.set_index(normalized, new_node(obj.clone())),
-            //     _ => false,
-            // },
-        }
-    }
-    fn set_index(&mut self, index: usize, node_ptr: Rc<RefCell<Node<T>>>) -> bool {
-        match &self.next[index] {
-            Some(_) => false,
-            None => {
-                self.next[index] = Some(node_ptr);
-                true
-            }
+                _ => {
+                    self.next[normalized].replace(empty_node());
+                    self.normalize_char_push(index, rest, normalize_num, obj)
+                }
+            },
         }
     }
     fn set_object(&mut self, obj: Rc<RefCell<T>>) -> bool {
@@ -97,10 +90,49 @@ impl<T> Node<T> {
             }
         }
     }
-    fn get_object(&mut self) -> (bool, Option<Rc<RefCell<T>>>) {
-        match &self.object {
-            Some(a) => (true, Some(a.clone())),
-            None => (false, None),
+    pub fn get(&self, s: String) -> (ERROR, Option<Rc<RefCell<T>>>) {
+        match s.get(..1) {
+            Some(a) => match s.get(1..) {
+                Some(b) => self.char_switch_get(a, b),
+                None => self.char_switch_get(a, ""),
+            },
+            None => (ERROR::NO_INDEX, None),
         }
     }
+    fn char_switch_get(&self, index: &str, rest: &str) -> (ERROR, Option<Rc<RefCell<T>>>) {
+        match index.chars().nth(0).unwrap() {
+            //If our index character is a capital letter
+            'A'..='Z' => self.normalize_char_get(index, rest, 65),
+            //If our index character is a capital letter
+            'a'..='z' => self.normalize_char_get(index, rest, 71),
+            _ => (ERROR::UNSUPPORTED_CHARS, None),
+        }
+    }
+    fn normalize_char_get(
+        &self,
+        index: &str,
+        rest: &str,
+        normalize_num: usize,
+    ) -> (ERROR, Option<Rc<RefCell<T>>>) {
+        let normalized = (index.chars().nth(0).unwrap() as usize) - normalize_num;
+        println!(" {}", normalized);
+        match self.next[normalized].clone() {
+            //If our selected node already exists
+            Some(node) => match rest.len() {
+                0 => (ERROR::NONE, node.borrow().object.clone()),
+                _ => node.borrow().get(rest.to_string()),
+            },
+            // if our selected node doesn't already exist
+            None => (ERROR::NULL_POINTER, None),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ERROR {
+    NONE,
+    NO_INDEX,
+    UNSUPPORTED_CHARS,
+    NULL_POINTER,
+    POINTER_SET,
 }
--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -4,7 +4,10 @@ mod hashmap;
 use std::{cell::RefCell, option::Option, rc::Rc};
 fn main() {
     let world = Rc::new(RefCell::new("World".to_string()));
+    let hello = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let n = hashmap::empty_node::<String>();
-    n.borrow_mut().push(String::from("hello"), world);
-    println!("{:#?}", n);
+    n.borrow_mut().push(String::from(hello), world);
+    let (err, res) = n.borrow().get(String::from(hello));
+    println!("{:#?}", err);
+    println!("{:#?}", res);
 }
